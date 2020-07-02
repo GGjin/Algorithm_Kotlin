1a5f866c92d13a6d1eb1eda7cf200b42e7594de4
GGjin <gg.jin.yu@gmail.com>
2020-07-02 16:44:24 +0800

    两数之和

---
+++ src/easy/TwoNum.kt
@@ -0,0 +1,43 @@
+package easy
+
+import java.util.*
+
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+ 
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = mutableMapOf<Int, Int>()
+    nums.forEachIndexed { index, i ->
+        if (map.containsKey(target - nums[index])) {
+            return intArrayOf(map[target - nums[index]] ?: 0, i)
+        }
+        map[nums[index]] = i
+    }
+    return intArrayOf(-1, -1)
+}
+
+fun main() {
+    val nums = intArrayOf(2, 7, 11, 15)
+    val target = 9
+    val result = twoSum(nums, target)
+    println(result::class.java)
+    println(result)
+}
+
